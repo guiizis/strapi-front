24d7862e6519597462b98267deb0c212a22227e9
guiziis <guigamoda@hotmail.com>
2023-08-09 21:10:19 -0300

    feat: adiciona globalStyle

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './templates/App';
 import { GlobalStyles } from './styles/global-styles';
+import { theme } from './styles/theme';
+import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
-    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <Home />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
