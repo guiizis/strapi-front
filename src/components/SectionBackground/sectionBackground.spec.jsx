c77931b5da8bfac04e4d9d9684626d9a947d9098
guiziis <guigamoda@hotmail.com>
2023-10-04 09:05:56 -0300

    test: adiciona testes para sectionBackground e sectionContainer

--- src/components/SectionBackground/sectionBackground.spec.jsx
+++ src/components/SectionBackground/sectionBackground.spec.jsx
@@ -1,10 +1,34 @@
-import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
 import { SectionBackground } from '.';
+import { theme } from '../../styles/theme';
 
 describe('<SectionBackground />', () => {
-  it('should render', () => {
-    renderTheme(<SectionBackground>Children</SectionBackground>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render with dark background', () => {
+    const { container } = renderTheme(
+      <SectionBackground background={true}>
+        <h1>Children</h1>
+      </SectionBackground>,
+    );
+    expect(container.firstChild).toHaveStyle({
+      background: `${theme.colors.primaryColor}`,
+    });
+  });
+
+  it('should render with light background', () => {
+    const { container } = renderTheme(
+      <SectionBackground background={false}>
+        <h1>Children</h1>
+      </SectionBackground>,
+    );
+    expect(container.firstChild).toHaveStyle({
+      background: `${theme.colors.white}`,
+    });
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(
+      <SectionBackground>Children</SectionBackground>,
+    );
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
